48155b4eb24cf2afdd5780814fa1c1f698deb726
Samuel Bruin <samuelmbruin@yahoo.com>
2025-01-20 22:40:52 +0100

    Add export and import api functions for demkit

--- src/api/demkit.rs
+++ src/api/demkit.rs
@@ -1,9 +1,12 @@
-use std::sync::OnceLock;
+use std::{num::ParseFloatError, str::FromStr, sync::OnceLock};
 
+use num_complex::{Complex, ParseComplexError};
+use once_cell::sync::Lazy;
 use reqwest::{self, Error};
 use serde::Deserialize;
+use regex::Regex;
 
-const BASE_URL: &str = "http://localhost:4000";
+const BASE_URL: &str = "http://localhost:5000";
 
 static CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
 
@@ -13,33 +16,72 @@ pub fn init() -> reqwest::Client {
 
 #[derive(Deserialize)]
 pub struct Measurement {
-    pub value: i32,
+    pub value: f64,
     pub unit: String,
 }
 
-pub async fn get_energy_import() -> Result<Measurement, Error> {
+
+#[derive(Deserialize)]
+struct Commodities {
+    #[serde(rename = "ELECTRICITY")]
+    pub electricity: String,
+}
+
+
+#[derive(thiserror::Error, Debug)]
+pub enum ApiError {
+    #[error("Request failed")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("Serde error")]
+    SerdeError(#[from] serde_json::Error),
+    #[error("Parse error")]
+    ParseError(#[from] ParseComplexError<ParseFloatError>),
+}
+
+// static COMPLEX_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"__\(([-+]?\d+\.?\d*)([-+]\d+\.?\d*)j\)").unwrap() );
+
+fn parse_complex_str(input: &str) -> Result<Complex<f64>, ParseComplexError<ParseFloatError>> {
+    Complex::from_str(&input[2..].replace("(", "").replace(")", ""))
+}
+
+pub async fn get_energy_import(house_id: u32) -> Result<Measurement, ApiError> {
     let client= CLIENT.get_or_init(init);
 
-    let url = format!("{}/energy/import", BASE_URL);
+    let url = format!("{}/get/SmartMeter-House-{house_id}/consumption", BASE_URL);
 
     let response = client
         .get(url)
         .send()
         .await?;
 
-    let response_body = response.json::<Measurement>().await?;
+    let response_body = response.json::<Commodities>().await?;
 
-    Ok(response_body)
+    let power = parse_complex_str(&response_body.electricity)?;
+
+    Ok(Measurement { value: power.norm().max(0.0), unit: String::from("W") })
 }
 
-pub async fn get_energy_export() -> Result<i32, Error> {
+pub async fn get_energy_export(house_id: u32) -> Result<Measurement, ApiError> {
+    let client= CLIENT.get_or_init(init);
+
+    let url = format!("{}/get/SmartMeter-House-{house_id}/consumption", BASE_URL);
+
+    let response = client
+        .get(url)
+        .send()
+        .await?;
+
+    let response_body = response.json::<Commodities>().await?;
+
+    let power = parse_complex_str(&response_body.electricity)?;
+
+    Ok(Measurement { value: power.norm().min(0.0), unit: String::from("W") })
+}
+
+pub async fn get_battery_status(house_id: u32) -> Result<Measurement, ApiError> {
     todo!("")
 }
 
-pub async fn get_battery_status() -> Result<i32, Error> {
-    todo!("")
-}
-
-pub async fn get_device_consumption(device_name: &str) -> Result<i32, Error> {
+pub async fn get_device_consumption(house_id: u32, device_name: &str) -> Result<Measurement, ApiError> {
     todo!("")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
+use api::demkit::get_energy_import;
 use env_logger::Env;
 
 mod api;
@@ -12,6 +13,16 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/test")]
+async fn test() -> impl Responder {
+
+    let import = get_energy_import(0).await.unwrap();
+
+    let (energy, unit) = (import.value, import.unit);
+
+    HttpResponse::Ok().body(format!("Energy: {energy} {unit}"))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
@@ -21,6 +32,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .service(hello)
+            .service(test)
             .configure(house::configure)
     })
     .bind(("127.0.0.1", 8080))?
