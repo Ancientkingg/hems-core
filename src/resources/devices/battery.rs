ed85038efea47671416d39fa4f3090c14137c21d
Samuel Bruin <samuelmbruin@yahoo.com>
2025-02-09 13:28:05 +0100

    Add battery structs

--- src/resources/devices/battery.rs
+++ src/resources/devices/battery.rs
@@ -1,10 +1,29 @@
 use actix_web::{get, web, HttpResponse, Responder};
+use serde::Serialize;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/battery/{id}").service(get_by_id));
 }
 
+#[derive(Serialize)]
+enum BatteryStatus {
+    Charging,
+    Discharging,
+    Idle,
+}
+
+#[derive(Serialize)]
+struct BatteryInfo {
+    capacity: f64,
+    max_charge: f64,
+    max_discharge: f64,
+    state_of_charge: f64,
+    status: BatteryStatus,
+    consumption: f64,
+}
+
 #[get("")]
-async fn get_by_id(house_id: web::Path<u32>, battery_id: web::Path<u32>) -> impl Responder {
+async fn get_by_id(id: web::Path<(u32, u32)>) -> impl Responder {
+    let (house_id, battery_id) = id.into_inner();
     HttpResponse::Ok().body(format!("Battery {battery_id} in house {house_id}"))
 }
