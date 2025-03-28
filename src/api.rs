24502a684f90f0752dcdb6ee259d27e5a61169a7
Neel Lodha <neellodha42@gmail.com>
2025-03-28 12:50:54 +0100

    fix formatting

--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,6 @@
-use crate::api::demkit::ha_entity;
-use crate::api::ha::entity;
-use actix_web::{web, HttpResponse, Responder};
-
 pub mod demkit;
-pub mod ha;
 
 #[actix_web::get("/healthz")]
 pub async fn health() -> impl actix_web::Responder {
     actix_web::HttpResponse::Ok().body("OK")
 }
-
-
